6f1b66ca25f68cdcd98078d9aaffb6b525300578
virak.ran <virakcambodia44@gmail.com>
2021-07-01 21:38:04 +0700

    chore: user logout

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,25 +38,13 @@
       </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      <v-btn color="warning" @click="logout"> LOGOUT </v-btn>
     </v-app-bar>
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
@@ -89,6 +77,11 @@ export default Vue.extend({
       rightDrawer: false,
       title: 'EMS',
     }
+  },
+  methods: {
+    async logout(this: any) {
+      await this.$auth.logout()
+    },
   },
 })
 </script>
