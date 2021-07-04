0575e0ad0d7b5fc98870eff8941dde8b842493a7
virak.ran <virakcambodia44@gmail.com>
2021-07-04 17:03:50 +0700

    chore: improvement

--- mixins/permission.ts
+++ mixins/permission.ts
@@ -1,7 +1,10 @@
 export default {
   methods: {
     isAdmin(this: any) {
-      if (this.$auth.user.authorities[0].authority === 'ROLE_ADMIN') {
+      if (
+        this.$auth.user.authorities.length > 0 &&
+        this.$auth.user.authorities[0].authority === 'ROLE_ADMIN'
+      ) {
         return true
       }
       return false
